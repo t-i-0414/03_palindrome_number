7a85f25d2e4cf53bb49a93686fa6a8e19507c596
iwashiro-takuya <gutlo.ol0414@gmail.com>
2021-06-20 18:22:25 +0900

    Update index.ts

---
+++ index.ts
@@ -0,0 +1,21 @@
+const isPalindrome = (x: number): boolean => {
+  if (Math.sign(x) === -1) {
+    return false;
+  }
+
+  const arr = `${x}`.split("");
+  const times = Math.floor(arr.length / 2);
+
+  let isPalindrome = true;
+
+  for (let i = 0; i < times; i += 1) {
+    if (arr[i] !== arr[arr.length - 1 - i]) {
+      isPalindrome = false;
+      break;
+    }
+  }
+
+  return isPalindrome;
+};
+
+export default isPalindrome;
